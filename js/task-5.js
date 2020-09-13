6f7cdcc08eab946acc56bd0ac232d065d1e8a46d
Roman Dobrodii <romariosi007@gmail.com>
2020-09-13 16:29:19 +0300

    Добавляет решение задачи №5

--- js/task-5.js
+++ js/task-5.js
@@ -114,16 +114,57 @@
 
 class Car {
   // Write code under this line
+  static getSpecs(car) {
+    const { maxSpeed, speed, isOn, distance, _price } = car;
+    return `maxSpeed: ${maxSpeed}, speed: ${speed}, isOn: ${isOn}, distance: ${distance}, price: ${_price}`;
+  }
 
-  getSpecs() {}
-  constructor() {}
-  get price() {}
-  set price(value) {}
-  turnOn() {}
-  turnOff() {}
-  accelerate(value) {}
-  decelerate(value) {}
-  drive(hours) {}
+  constructor({ speed = 0, price, maxSpeed, isOn = false, distance = 0 } = {}) {
+    this.speed = speed;
+    this._price = price;
+    this.maxSpeed = maxSpeed;
+    this.isOn = isOn;
+    this.distance = distance;
+  }
+
+  get price() {
+    return this._price;
+  }
+
+  set price(value) {
+    this._price = value;
+  }
+
+  turnOn() {
+    this.isOn = true;
+  }
+
+  turnOff() {
+    this.isOn = false;
+    this.speed = 0;
+  }
+
+  accelerate(value) {
+    if (this.speed + value <= this.maxSpeed) {
+      this.speed += value;
+    } else {
+      this.speed = this.maxSpeed;
+    }
+  }
+
+  decelerate(value) {
+    if (this.speed - value > 0) {
+      this.speed -= value;
+    } else {
+      this.speed = 0;
+    }
+  }
+
+  drive(hours) {
+    if (this.isOn) {
+      this.distance += hours * this.speed;
+    }
+  }
 }
 
 const mustang = new Car({ maxSpeed: 200, price: 2000 });
